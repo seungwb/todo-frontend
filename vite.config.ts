d65833cc16e474814affee4e0cdc9114eda1aec8
seungwb <seungwb@gmail.com>
2025-02-01 17:53:50 +0900

    refactor: tailwindcss CSS 프레임워크 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react()
+        , tailwindcss()
+        ,
+    ],
     server: {
         port: 3000, // CRA와 동일한 포트 유지
     },
